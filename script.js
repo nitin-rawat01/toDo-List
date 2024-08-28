53408956467075c8a9d9ea8633abf1cbf3956803
nitin-rawat01 <nitrawat2499@gmail.com>
2024-08-28 17:42:08 +0530

    changes in user interface

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+let form =  document.getElementById('form');
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+     userInput = document.querySelector('#task').value;
+    console.log(userInput);
+})
 
